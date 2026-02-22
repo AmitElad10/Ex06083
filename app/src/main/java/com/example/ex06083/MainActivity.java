39ed0a9acc7560b2aff450a2986ea1d10bf0da63
AmitElad10 <amitelad2010@gmail.com>
2026-02-22 21:04:07 +0200

    finish working on sub op.

--- app/src/main/java/com/example/ex06083/MainActivity.java
+++ app/src/main/java/com/example/ex06083/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     int fnum = 0;
     int op = 0;
     int num;
+    boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +27,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void plus(View view) {
-        op = 1 ;
-        String n = eTV.getText(). toString();
+        op = 1;
+        String n = eTV.getText().toString();
         num = Integer.parseInt(n);
         eTV.setText("");
         fnum += num;
 
     }
+
     public void equal(View view) {
-        if(op == 1){
-            String n = eTV.getText(). toString();
-            num = Integer.parseInt(n);
+        String n = eTV.getText().toString();
+        num = Integer.parseInt(n);
+        if (op == 1) {
             fnum += num;
+        } else if (op == 2) {
+            fnum = fnum - num;
         }
         String x = Integer.toString(fnum);
         eTV.setText(x);
@@ -47,5 +51,22 @@ public class MainActivity extends AppCompatActivity {
     public void clean(View view) {
         fnum = 0;
         eTV.setText("");
+    }
+
+    public void sub(View view) {
+        op = 2;
+        String n = eTV.getText().toString();
+        num = Integer.parseInt(n);
+        eTV.setText("");
+        if(flag == true){
+            fnum = num;
+            flag = false ;
+        }
+        else{
+            fnum = fnum - num;
+        }
+
+
+
     }
 }
