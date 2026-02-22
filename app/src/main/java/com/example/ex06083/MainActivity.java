70ac1b96cd100717aa0e6423a6ec574d63be91f5
AmitElad10 <amitelad2010@gmail.com>
2026-02-22 20:23:51 +0200

    finish working on addition op.

--- app/src/main/java/com/example/ex06083/MainActivity.java
+++ app/src/main/java/com/example/ex06083/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex06083;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText eTV;
+    int fnum = 0;
+    int op = 0;
+    int num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        eTV = findViewById(R.id.eTV);
+
+    }
+
+    public void plus(View view) {
+        op = 1 ;
+        String n = eTV.getText(). toString();
+        num = Integer.parseInt(n);
+        eTV.setText("");
+        fnum += num;
+
+    }
+    public void equal(View view) {
+        if(op == 1){
+            String n = eTV.getText(). toString();
+            num = Integer.parseInt(n);
+            fnum += num;
+        }
+        String x = Integer.toString(fnum);
+        eTV.setText(x);
+
+    }
+
+    public void clean(View view) {
+        fnum = 0;
+        eTV.setText("");
     }
 }
